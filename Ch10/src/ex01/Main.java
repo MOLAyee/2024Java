72d9d6e181cb3851c5c33501bfc1e14837851c11
MOLAyee <169133359+MOLAyee@users.noreply.github.com>
2024-11-26 10:42:51 +0900

    Add files via upload

---
+++ Ch10/src/ex01/Main.java
@@ -0,0 +1,27 @@
+package ex01;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("temp.txt");
+//        try {
+//            FileReader reader = new FileReader("temp.txt");
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//
+//        }
+//
+//        try {
+//            FileReader reader1 = new FileReader("temp.txt");
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
+        FileReader reader = new FileReader("temp.txt");
+
+    }
+}
